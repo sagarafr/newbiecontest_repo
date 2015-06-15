fa11b12508df49a031004dc191af58f7e5d6c60e
sagarafr <sagarafr@gmail.com>
2015-06-15 17:51:08 +0200

    Login class with session method

---
+++ programmation/login.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import requests
+import getpass
+
+class Login:
+	def __init__(self,user='',password=''):
+		self.session=requests.session()
+		self.login=dict()
+		self.url='https://www.newbiecontest.org/forums/index.php?action=login2'
+		self.connection(user,password)
+
+	def connection(self,user='',password=''):
+		if user == '':
+			user=input("User : ")
+
+		if password == '':
+			password=getpass.getpass("Password : ")
+
+		self.login=dict(user=user,passwrd=password)
+		self.session.post(self.url,data=self.login)
+
+	def getSession(self):
+		return self.session
+
+	def getContent(self,addr):
+		return str(self.session.get(addr).content)
+
+	def sendAnswer(self,addr):
+		return str(self.session.post(addr).content)
