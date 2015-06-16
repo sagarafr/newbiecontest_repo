a5970648f3db56d7284613594ef08dfe4d393933
sagarafr <sagarafr@gmail.com>
2015-06-16 17:59:08 +0200

    Quand le jazz est la challenge done

---
+++ programmation/quand_le_jazz_est_la.java
@@ -0,0 +1,15 @@
+public class quand_le_jazz_est_la {
+	public static void main(String[] args) {
+		int i = 0;
+		for (int k= 0 ; k==128 ; k++) {
+			i = k*k + 17;
+		}
+		System.out.println(" i : " + i);
+		String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		StringBuffer buffer = new StringBuffer();
+		for (int k = 0;k<str.length()/3;k++) {
+			buffer.append(str.substring(k+2,k+3));
+		}
+		System.out.println("Resultat : " + buffer.length());
+	}
+}
